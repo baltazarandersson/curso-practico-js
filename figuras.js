25c692554baa2511a75aac5523ca97fe35efe8f8
Baltazar <balti.kpo@gmail.com>
2022-02-04 14:43:04 -0300

    Agregada interfaz en html y display de resultado

--- figuras.js
+++ figuras.js
@@ -1,49 +1,17 @@
-console.group("Cuadrado")
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden " + ladoCuadrado)
-
 const perimetroCuadrado = lado => lado * 4
-console.log("El perimetro del cuadrado es " + perimetroCuadrado(ladoCuadrado))
-
 const areaCuadrado = lado => lado * lado
-console.log("El area del cuadrado es " + areaCuadrado(ladoCuadrado))
-console.groupEnd();
 
-console.group("Triangulo")
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
 const altTriangulo = 5.5;
-
-console.log(
-    "Los lados del Triangulo miden " 
-    + ladoTriangulo1 
-    + " " 
-    + ladoTriangulo2 
-    + " " 
-    + baseTriangulo
-    )
-
 const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
-console.log(`El perimetro del Triangulo es ${perimetroTriangulo(ladoTriangulo1, ladoTriangulo2, baseTriangulo)}`)
-
 const areaTriangulo = (base, altura) => ((base * altura) / 2)
-console.log(`"El area del Triangulo es ${areaTriangulo(baseTriangulo, altTriangulo)}`)
-console.groupEnd();
 
-console.group('Circulo')
-const radioCirculo = 4
 const diametroCirculo = (radio) => radio * 2
 const PI = Math.PI
 const perimetroCirculo = (radio, PI) => diametroCirculo(radio) * PI
 const areaCirculo = (radio, PI) => (radio * radio) * PI
-
-console.log(`El radio del circulo es ${radioCirculo}`)
-console.log(`El diametro del circulo es ${diametroCirculo(radioCirculo)}`)
-console.log(`PI es ${PI}`)
-console.log(`El perimetro del circulo es ${perimetroCirculo(radioCirculo, PI)} cm`)
-console.log(`El area del circulo es ${areaCirculo(radioCirculo, PI)} cm2`)
-console.groupEnd();
 
 function calcularPerimetroCuadrado() {
     const input = document.getElementById("InputCuadrado")
@@ -56,5 +24,39 @@ function calcularAreaCuadrado() {
     const input = document.getElementById("InputCuadrado")
     const value = input.value
     const area = areaCuadrado(value)
+    alert(area) 
+}
+
+function calcularPerimetroTriangulo() {
+    const input = document.getElementById("InputTrianguloLado1")
+    const input2 = document.getElementById("InputTrianguloLado2")
+    const input3 = document.getElementById("InputTrianguloLado3")
+    const value = Number(input.value)
+    const value2 = Number(input2.value)
+    const value3 = Number(input3.value)
+    const perimetro = perimetroTriangulo(value, value2, value3)
+    alert(perimetro)
+}
+ 
+function calcularAreaTriangulo() {
+    const input3 = document.getElementById("InputTrianguloLado3")
+    const input4 = document.getElementById("InputTrianguloLado3")
+    const value3 = Number(input3.value)
+    const value4 = Number(input4.value)
+    const area = areaTriangulo(value3, value4)
+    alert(area) 
+}
+
+function calcularPerimetroCirculo() {
+    const input = document.getElementById("InputCirculo")
+    const value = Number(input.value)
+    const perimetro = perimetroCirculo(value, PI)
+    alert(perimetro)
+}
+ 
+function calcularAreaCirculo() {
+    const input = document.getElementById("InputCirculo")
+    const value = Number(input.value)
+    const area = areaCirculo(value, PI)
     alert(area) 
 }
